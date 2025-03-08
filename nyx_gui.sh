089aad6d97c19f0ad2846d25c5b14601e08b483e
Fabio Cutaia <fabio.cutaia@gmail.com>
2025-03-08 09:17:30 +0100

    Step

---
+++ nyx_gui.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SETTINGS_CONFIG="/home/nyx_gui/config/appsettings.json"
+SETTINGS_BIN="/home/nyx_gui/bin/appsettings.json"
+
+if cmp -s "$SETTINGS_CONFIG" "$SETTINGS_BIN"; then
+    echo 'Same files'
+else
+    echo 'Different files'
+    cp $SETTINGS_CONFIG $SETTINGS_BIN
+fi
+cd /home/nyx_gui/bin
+/home/nyx_gui/dotnet/dotnet /home/nyx_gui/bin/NyxGUI.dll
