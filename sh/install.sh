1e00825f45da0d8a4750adfad6540c9ec5a92d80
Fabio Cutaia <fabio.cutaia@gmail.com>
2025-03-15 07:11:30 +0100

    Step

--- sh/install.sh
+++ sh/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt install wget
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
